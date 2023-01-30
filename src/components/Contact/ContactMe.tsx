fd69b7bfca21e0085a763771546f18cbf09587af
Brandon Learned <90928439+blearned92@users.noreply.github.com>
2023-01-30 14:10:14 -0700

    Finished the Contact me section design.
    Still needs to send forms, and the resume button needs to link to an actual resume.

--- src/components/Contact/ContactMe.tsx
+++ src/components/Contact/ContactMe.tsx
@@ -1,6 +1,50 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 import "./ContactMe.scss";
 
 const ContactMe = () => {
+
+    const [messageSent, setMessageSent] = useState(false);
+    const [name, setName] = useState("");
+
+    const handleSubmit = () => {
+        if(messageSent){
+            setMessageSent(false);
+        } else {
+            setMessageSent(true);
+        }
+    }
+
+    const handleReturnForm = () => {
+        if(messageSent){
+            return(
+                <>
+                    <h2>Your email has been successfully submitted!</h2>
+                    <Button onClick={handleSubmit}>Return</Button>
+                </>
+                
+            )
+        } else {
+            return(
+                    <form action="https://formsubmit.co/blearned92@gmail.com" method="POST">
+                        <section className="form-section">
+                            <div className="form-left">
+                                <label>Name</label>
+                                <input type='text' name="name" placeholder="Input Name Here" />
+                                <label>Email Address</label>
+                                <input type='email' name="email" placeholder="Input Email Address Here" />
+                            </div>
+                            <div className="form-right">
+                                <label>Message</label>
+                                <textarea name="message" placeholder="Input Message Here" />
+                            </div>  
+                        </section>
+                        <Button type="submit" onClick={handleSubmit}>Submit</Button>
+                    </form>
+            )
+        }
+    }
+
     return(
         <section className="contact-me-wrapper" id="contact-me">
             <aside className="background-left">
@@ -10,9 +54,11 @@ const ContactMe = () => {
             <section className="contact-me-info">
                 <h1>Contact Me</h1>
                 <div className="underline-decoration"></div>
-                <div className="content-box">
-                   
-                </div>
+                {/* <div className="content-box"> */}
+                {
+                    handleReturnForm()
+                }
+                {/* </div> */}
             </section>
         </section>
     )
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,35 +2,22 @@ import styled from "styled-components";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-
-const Wrapper = styled.div`
-    background-color: purple;
-    display: flex;
-    width: 100%;
-    height: 42px;
-    justify-content: right;
-    align-items: center;
-`;
-
-const Logo = styled.img`
-    height: 100%;
-    width: 100%;
-    padding-right: 100%;
-`;
-
-const FooterIcon = styled(FontAwesomeIcon)`
-    color: ;
-    height: 60%;
-    padding-right: 10px;
-    cursor: pointer;
-`
+import "./Footer.scss";
 
 const Footer = () => {
     return(
-        <Wrapper>
-            <FooterIcon icon={faLinkedin} onClick={() => window.open("https://www.linkedin.com/in/brandon-learned/")}/>
-            <FooterIcon icon={faGithub} onClick={() => window.open("https://github.com/blearned92/")}/>
-        </Wrapper>
+        <section className="wrapper">
+            <span>
+                <a href="https://www.freepik.com/free-vector/abstract-black-texture-background-hexagon_18702139.htm#query=black%20hexagon&position=2&from_view=keyword"> Background Image by almumtazza</a> on Freepik
+            </span>
+            <span className="name">
+                Brandon Learned 2023
+            </span>
+            <ul className="footer-ul">
+                <li><FontAwesomeIcon icon={faLinkedin} onClick={() => window.open("https://www.linkedin.com/in/brandon-learned/")}/></li>
+                <li><FontAwesomeIcon icon={faGithub} onClick={() => window.open("https://github.com/blearned92/")}/></li>
+            </ul>
+        </section>
     )
 }
 
